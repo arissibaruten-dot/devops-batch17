0aaed13c49a76fcb826b761abd08c89a41b5f878
arissibaruten-dot <arissibaruten@gmail.com>
2026-09-05 12:02:08 +0800

    Test methods in TestMessageUtil

--- src/test/java/TestMessageUtil.java
+++ src/test/java/TestMessageUtil.java
@@ -4,19 +4,19 @@ import static org.junit.Assert.assertEquals;
 
 public class TestMessageUtil {
 
-    String message = "Robert";
-    MessageUtil messageUtil = new MessageUtil(message);
+  String message = "Robert";    
+  MessageUtil messageUtil = new MessageUtil(message);
+   
+  @Test
+  public void testPrintMessage() {      
+    System.out.println("Inside testPrintMessage()");     
+    assertEquals(message,messageUtil.printMessage());
+  }
 
-    @Test
-    public void testPrintMessage() {
-        System.out.println("Inside testPrintMessage()");
-        assertEquals(message, messageUtil.printMessage());
-    }
-
-    @Test
-    public void testSalutationMessage() {
-        System.out.println("Inside testSalutationMessage()");
-        message = "Hi!" + "Robert";
-        assertEquals(message, messageUtil.salutationMessage());
-    }
+  @Test
+  public void testSalutationMessage() {
+    System.out.println("Inside testSalutationMessage()");
+    message = "Hi!" + "Robert";
+    assertEquals(message,messageUtil.salutationMessage());
+  }
 }
